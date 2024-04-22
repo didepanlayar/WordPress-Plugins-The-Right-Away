9dc12d452acb00dc21f968fd52f1bc7d9554eaee
Di Depan Layar <albinmaulana@yahoo.com>
2024-04-22 20:46:55 +0700

    Feature: Structuring the Plugin

---
+++ My Simple Slider/my-simple-slider.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Plugin Name: My Simple Slider
+ * Plugin URI: https://wordpress.org/plugins/my-simple-slider
+ * Description: My Simple Slider includes the following options for your content sliding needs. Just include the ones you want to change.
+ * Version: 1.0
+ * Requires at least: 5.6
+ * Author: Di Depan Layar
+ * Author URI: https://didepanlayar.com
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: my-simple-slider
+ * Domain Path: /languages
+ */
+
+ /*
+My Simple Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+My Simple Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with My Simple Slider. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
+
+if( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
