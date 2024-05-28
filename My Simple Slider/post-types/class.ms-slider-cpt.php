b8f5ce23c9968b667fe6590ed4cf759963c1b686
Di Depan Layar <albinmaulana@yahoo.com>
2024-05-28 14:12:34 +0700

    Feature: Metabox API - Save Metabox Data

--- My Simple Slider/post-types/class.ms-slider-cpt.php
+++ My Simple Slider/post-types/class.ms-slider-cpt.php
@@ -5,6 +5,7 @@ if( ! class_exists( 'MS_Slider_Post_Type' ) ) {
         function __construct() {
             add_action( 'init', array( $this, 'create_post_type' ) );
             add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+            add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
         }
 
         function create_post_type() {
@@ -49,6 +50,18 @@ if( ! class_exists( 'MS_Slider_Post_Type' ) ) {
 
         public function add_inner_meta_boxes( $post ) {
             require_once( MS_SLIDER_PATH . 'views/ms-slider_metabox.php' );
+        }
+
+        public function save_post( $post_id ) {
+            if( isset( $_POST['action'] ) && $_POST['action'] == 'editpost' ) {
+                $old_link_text  = get_post_meta( $post_id, 'ms_slider_link_text', true );
+                $new_link_text  = $_POST['ms_slider_link_text'];
+                $old_link_url   = get_post_meta( $post_id, 'ms_slider_link_url', true );
+                $new_link_url   = $_POST['ms_slider_link_url'];
+
+                update_post_meta( $post_id, 'ms_slider_link_text', $new_link_text, $old_link_text );
+                update_post_meta( $post_id, 'ms_slider_link_url', $new_link_url, $old_link_url );
+            }
         }
     }
 }
