98e06f501fba9aeeb8a903e0f63e330f71e845a0
Di Depan Layar <albinmaulana@yahoo.com>
2025-03-19 17:06:22 +0700

    Feature: Settings and Options API - Split Sections into Tabs

--- My Simple Slider/views/settings-page.php
+++ My Simple Slider/views/settings-page.php
@@ -1,10 +1,19 @@
 <div class="wrap">
     <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+    <?php $active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'main_options'; ?>
+    <h2 class="nav-tab-wrapper">
+        <a href="?page=ms-slider-setting&tab=main_options" class="nav-tab <?php echo $active_tab == 'main_options' ? 'nav-tab-active' : ''; ?>">Main</a>
+        <a href="?page=ms-slider-setting&tab=other_options" class="nav-tab <?php echo $active_tab == 'other_options' ? 'nav-tab-active' : ''; ?>">Other</a>
+    </h2>
     <form action="options.php" method="POST">
         <?php
-            settings_fields( 'ms_slider_group' );
-            do_settings_sections( 'ms_slider_page_1' );
-            do_settings_sections( 'ms_slider_page_2' );
+            if( $active_tab == 'main_options' ) {
+                settings_fields( 'ms_slider_group' );
+                do_settings_sections( 'ms_slider_page_1' );
+            } else {
+                settings_fields( 'ms_slider_group' );
+                do_settings_sections( 'ms_slider_page_2' );
+            }
             submit_button( 'Save Settings' );
         ?>
     </form>
